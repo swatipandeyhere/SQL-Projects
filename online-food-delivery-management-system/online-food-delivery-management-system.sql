5214f93cfffe0fa28a55783d73aeb72fabb089cd
Swati Pandey <swatipandeyhere@gmail.com>
2024-01-31 00:18:53 +0530

    Add Login Table With Inserted Values

--- online-food-delivery-management-system/online-food-delivery-management-system.sql
+++ online-food-delivery-management-system/online-food-delivery-management-system.sql
@@ -29,3 +29,31 @@ VALUES
 -- View Contents of Customer Table
 
 SELECT * FROM tbl_Customer;
+
+-- Create Login Table
+
+CREATE TABLE tbl_Login(UserName VARCHAR(15) PRIMARY KEY,
+CustomerId INT REFERENCES tbl_Customer(CustomerId),
+Password VARBINARY(20),
+Email VARCHAR(30) NOT NULL,
+CONSTRAINT tbl_Login_Email CHECK(Email LIKE '%@%.%'));
+
+-- Insert Values into Login Table
+
+INSERT INTO
+tbl_Login(UserName, CustomerId, Password, Email)
+VALUES
+('sunita.sharma', 100, HASHBYTES('MD5', 'password00'), 'sunita.sharma@gmail.com'),
+('ashok.singhal', 101, HASHBYTES('MD5', 'password01'), 'ashok.singhal@gmail.com'),
+('rohit.rana', 102, HASHBYTES('MD5', 'password02'), 'rohit.rana@gmail.com'),
+('jyoti.lamba', 103, HASHBYTES('MD5', 'password03'), 'jyoti.lamba@gmail.com'),
+('sumit.vats', 104, HASHBYTES('MD5', 'password04'), 'sumit.vats@gmail.com'),
+('manoj.kaushik', 105, HASHBYTES('MD5', 'password05'), 'manoj.kaushik@gmail.com'),
+('chetan.gupta', 106, HASHBYTES('MD5', 'password06'), 'chetan.gupta@gmail.com'),
+('madhuri.thakkar', 107, HASHBYTES('MD5', 'password07'), 'madhuri.thakkar@gmail.com'),
+('sneha.nair', 108, HASHBYTES('MD5', 'password08'), 'sneha.nair@gmail.com'),
+('binod.goel', 109, HASHBYTES('MD5', 'password09'), 'binod.goel@gmail.com');
+
+-- View Contents of Login Table
+
+SELECT * FROM tbl_Login;
